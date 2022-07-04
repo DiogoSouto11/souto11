7a2c808ca2e5814fb937e83778e0ee81a4addfcb
Diogo Souto <diogosouto7c.abrotero@gmail.com>
2022-07-04 14:35:52 +0100

    Create about page

--- souto11/souto11.Web/Controllers/HomeController.cs
+++ souto11/souto11.Web/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace souto11.Web.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
